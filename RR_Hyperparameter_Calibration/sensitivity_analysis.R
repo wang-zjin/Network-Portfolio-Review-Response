16866f5f03128a2053100878e3ccdb232534a262
Zijin Wang <495260464@qq.com>
2025-02-07 00:29:35 -0800

    Create sensitivity_analysis.R

---
+++ RR_Hyperparameter_Calibration/sensitivity_analysis.R
@@ -0,0 +1,155 @@
+# Clear the workspace 
+rm(list = ls())
+
+# Set the working directory 
+setwd("~/Documents/GitHub/Network-Portfolio-Review-Response/RR_Hyperparameter_Calibration")
+
+# Load Functions and other Files
+source('./PackagesNetworkPortfolio.R')
+source('./FunctionsNetworkPortfolio.R')
+
+# load data
+prices<-read.csv("SP500 securities_up_20230306.csv")
+ZOO <- zoo(prices[,-1], order.by=as.Date(as.character(prices$Dates), format='%Y-%m-%d'))
+
+#return
+return<- Return.calculate(ZOO, method="log")
+return<- return[-1, ]
+returnstd<-xts(return)
+p=dim(return)[2]
+
+CORR = cor(returnstd)
+# Check positive-definiteness of CORR
+eigenvalues = eig(CORR)
+isPositiveDefinite = all(eigenvalues > 0)
+# So CORR is positive-definite
+
+mean(returnstd[,1])
+mean(returnstd[,3])
+mean(returnstd[,6])
+std(returnstd[,1])
+std(returnstd[,3])
+std(returnstd[,6])
+cor(returnstd[,c(1,3,6)])
+
+# Given data
+A <- CORR - diag(1,p,p)
+mu <- apply(returnstd, 2, mean)
+sigma <- apply(returnstd, 2, sd)
+Sigma <- diag(sigma) %*% CORR %*% diag(sigma)
+
+network_assets = network.correlation(returnstd)
+phi <- eigen_centrality(network_assets,directed = FALSE, scale = TRUE)$vector
+
+phi_star = quantile(phi, 0.1)
+mu_star = mean(mu)
+
+###### PLug-In 2 constraints network portfolio #####
+net.gmin.port = network.efficient.portfolio(phi, Sigma, phi_star,TRUE)
+w =net.gmin.port$weights
+w_network_2constraint_plugin<-w
+
+###### PLug-In 3 constraints network portfolio #####
+net.gmin.port = network.3constraint.portfolio(phi, mu, Sigma, phi_star, mu_star, TRUE)
+w =net.gmin.port$weights
+w_network_3constraint_plugin<-w
+
+# Number of observations to generate
+n_simu <- 500
+
+# Generate multivariate normal data
+set.seed(42) # for reproducibility
+data <- mvrnorm(n = n_simu, mu = mu, 
+                Sigma = Sigma)
+
+# Estimate mean vector
+estimated_mu <- colMeans(data)
+
+# Estimate correlation matrix
+estimated_A <- cor(data)
+
+# Estimate covariance matrix
+estimated_Sigma <- cov(data)
+
+# Estimate Eigenvector centrality
+EC_DS =linfun3_1(estimated_A-diag(1,p)-diag(max(eigen(estimated_A)$value),p),
+                 rep(0,p),
+                 lambda=0.45,
+                 abs(eigen(estimated_A-diag(1,p)-diag(max(eigen(estimated_A)$value),p))$vector[1,1])
+)
+EC_DS=EC_DS/max(EC_DS)
+
+##### Dantzig #######
+# lmd1 = estimate_dantzig_lambda1_portfolio_parameter_simplified(data, window_size=500)
+lmd1 = 0.3358
+lmd2 = 0.0001
+lmd3 = 0.1
+theta1 =linfun1(estimated_Sigma,rep(1,p),lambda=lmd1) # lambda <= 0.1 will lead to be infeasible
+theta2 =linfun1(estimated_Sigma,estimated_mu,lambda=lmd2) # lambda <= 0.1 will lead to be infeasible
+theta3 =linfun1(estimated_Sigma,EC_DS,lambda=lmd3) # lambda <= 0.1 will lead to be infeasible
+
+###### Dantzig 2 constraints network portfolio #####
+if((c(theta1/sum(theta1))%*%c(EC_DS))>phi_star){
+  alpha=c((sum(theta3)*sum(theta1)*phi_star-(sum(theta3))^2)/(EC_DS%*%theta3*sum(theta1)-(sum(theta3))^2))
+  w = alpha*theta3/sum(theta3)+(1-alpha)*theta1/sum(theta1)
+} else{
+  w = theta1/sum(theta1)
+}
+w[is.na(w)] <- 0
+w_2constraint_dantzig<-w
+
+###### Dantzig 3 constraints network portfolio #####
+if((c(theta1/sum(theta1))%*%c(EC_DS))>phi_star){
+  if((c(theta1/sum(theta1))%*%c(estimated_mu))<mean(estimated_mu)){
+    # 3 constraint case
+    M1 <- cbind(rbind(1,phi_star,mean(estimated_mu)),
+                rbind(sum(theta3),EC_DS%*%theta3,estimated_mu%*%theta3),
+                rbind(sum(theta2),EC_DS%*%theta2,estimated_mu%*%theta2))
+    M2 <- cbind(rbind(sum(theta1),EC_DS%*%theta1,estimated_mu%*%theta1),
+                rbind(1,phi_star,mean(estimated_mu)),
+                rbind(sum(theta2),EC_DS%*%theta2,estimated_mu%*%theta2))
+    M <- cbind(rbind(sum(theta1),EC_DS%*%theta1,estimated_mu%*%theta1),
+               rbind(sum(theta3),EC_DS%*%theta3,estimated_mu%*%theta3),
+               rbind(sum(theta2),EC_DS%*%theta2,estimated_mu%*%theta2))
+    gamma1 <- det(M1)/det(M)
+    gamma2 <- det(M2)/det(M)
+    alpha1 <- c(gamma1*sum(theta3))
+    alpha2 <- c(gamma2*sum(theta2))
+    w1 <- (1-alpha1-alpha2)*theta1/sum(theta1)
+    w2 <- alpha1*theta3/sum(theta3)
+    w3 <- alpha2*theta2/sum(theta2)
+    w = w1 +  w2 + w3
+  }
+  else{
+    # 1 centrality constraint case
+    alpha=c((sum(theta3)*sum(theta1)*phi_star-(sum(theta3))^2)/(EC_DS%*%theta3*sum(theta1)-(sum(theta3))^2))
+    w = alpha*theta3/sum(theta3)+(1-alpha)*theta1/sum(theta1)
+  }} else{
+    if((c(theta1/sum(theta1))%*%c(estimated_mu))<mean(estimated_mu)){
+      # 1 expected return constraint case 
+      alpha=c((sum(theta2)*sum(theta1)*mean(estimated_mu)-(sum(theta2))^2)/(estimated_mu%*%theta2*sum(theta1)-(sum(theta2))^2))
+      w = alpha*theta2/sum(theta2)+(1-alpha)*theta1/sum(theta1)
+    }
+    else{
+      # global minimum variance case 
+      w = theta1/sum(theta1)
+    }
+  }
+w[is.na(w)] <- 0
+w_3constraint_dantzig<-w
+
+# Function to compute the L2 (Euclidean) norm difference between two vectors
+l2_norm <- function(x, y) {
+  sqrt(sum((x - y)^2))
+}
+
+# Compare the 2-constraint portfolios
+l2_norm_2constraint <- l2_norm(w_2constraint_dantzig, w_network_2constraint_plugin)
+cat("L2 norm difference for 2-constraint portfolios:", l2_norm_2constraint, "\n")
+cat("L2 norm relative difference for 2-constraint portfolios:", l2_norm_2constraint/sqrt(sum(w_network_2constraint_plugin^2)), "\n")
+
+# Compare the 3-constraint portfolios
+l2_norm_3constraint <- l2_norm(w_3constraint_dantzig, w_network_3constraint_plugin)
+cat("L2 norm difference for 3-constraint portfolios:", l2_norm_3constraint, "\n")
+cat("L2 norm relative difference for 3-constraint portfolios:", l2_norm_3constraint/sqrt(sum(w_network_3constraint_plugin^2)), "\n")
+
